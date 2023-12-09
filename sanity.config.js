c97303a8feeedb2e1f69a4d871878697989e6781
Bulut Yerli <bulutyerli@gmail.com>
2023-12-09 18:37:35 +0300

    netlify fix

--- sanity.config.js
+++ sanity.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import { schemaTypes } from "@/schemas/index";
-import { dashboardTool } from "@sanity/dashboard";
-import { netlifyWidget } from "sanity-plugin-dashboard-widget-netlify";
 
 export default defineConfig({
   name: "default",
@@ -12,24 +10,7 @@ export default defineConfig({
   dataset: "production",
   basePath: "/studio",
 
-  plugins: [
-    deskTool(),
-    dashboardTool({
-      widgets: [
-        netlifyWidget({
-          title: "Photograph Portfolio",
-          sites: [
-            {
-              title: "Photograph Portfolio",
-              projectId: "gc6ah75d",
-              dataset: "production",
-              basePath: "/studio",
-            },
-          ],
-        }),
-      ],
-    }),
-  ],
+  plugins: [deskTool()],
 
   schema: {
     types: schemaTypes,
